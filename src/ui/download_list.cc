3cea38cd75813f0d2a4eec4a0aae9168da4cf4dc
Jesse Chan <jc@linux.com>
2021-02-16 23:23:00 +0800

    download_list: fix out-of-bound dereference when view is empty
    Fixes:
    
    ==18355==ERROR: AddressSanitizer: heap-buffer-overflow
    READ of size 8 at 0x60200000dbd8 thread T2 (rtorrent scgi)
        #0 0x55dcc3b00273 in ui::DownloadList::unfocus_download(core::Download*) src/ui/download_list.cc:106
        #1 0x55dcc3a1f812 in cmd_ui_unfocus_download(core::Download*) src/command_ui.cc:656

--- src/ui/download_list.cc
+++ src/ui/download_list.cc
@@ -103,8 +103,8 @@ DownloadList::unfocus_download(core::Download* d) {
       d == static_cast<Download*>(m_uiArray[DISPLAY_DOWNLOAD])->download())
     activate_display(DISPLAY_DOWNLOAD_LIST);
 
-  if (*current_view()->focus() == d &&
-      current_view()->focus() < current_view()->end_visible())
+  if (current_view()->focus() < current_view()->end_visible() &&
+      *current_view()->focus() == d)
     current_view()->next_focus();
 }
 
