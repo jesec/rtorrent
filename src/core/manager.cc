jesec/rtorrent

commit 2f286f939ad51a99883040eb57a7aa4f5c28c95d

apply_load: reply with hashes of added downloads for synchronous calls

src/core/manager.cc
@@ -412,15 +412,17 @@ Manager::receive_http_failed(std::string msg) {
   push_log_std("Http download error: \"" + msg + "\"");
 }
 
-void
+torrent::Object
 Manager::try_create_download(const std::string&       uri,
                              int                      flags,
                              const command_list_type& commands) {
+  auto result = torrent::Object::create_value();
+
   // If the path was attempted loaded before, skip it.
   if ((flags & create_tied) && !(flags & create_raw_data) &&
       !is_network_uri(uri) && !is_magnet_uri(uri) && !is_data_uri(uri) &&
       !file_status_cache()->insert(uri, flags & create_throw))
-    return;
+    return result;
 
   // Adding download.
   DownloadFactory* f = new DownloadFactory(this);
@@ -430,8 +432,16 @@ Manager::try_create_download(const std::string&       uri,
 
   f->set_start(flags & create_start);
   f->set_print_log(!(flags & create_quiet));
-  f->set_immediate(flags & create_throw);
-  f->slot_finished([f]() { delete f; });
+
+  if (flags & create_throw) {
+    f->set_immediate(true);
+    f->slot_finished([f, &result]() {
+      result = f->result();
+      delete f;
+    });
+  } else {
+    f->slot_finished([f]() { delete f; });
+  }
 
   if (flags & create_raw_data) {
     f->load_raw_data(uri);
@@ -448,6 +458,8 @@ Manager::try_create_download(const std::string&       uri,
   }
 
   f->commit();
+
+  return result;
 }
 
 void
@@ -511,13 +523,16 @@ manager_equal_tied(const std::string& path, Download* download) {
          rpc::call_command_string("d.tied_to_file", rpc::make_target(download));
 }
 
-void
+torrent::Object
 Manager::try_create_download_expand(const std::string& uri,
                                     int                flags,
                                     command_list_type  commands) {
+  torrent::Object             rawResult = torrent::Object::create_list();
+  torrent::Object::list_type& result    = rawResult.as_list();
+
   if (flags & create_raw_data) {
-    try_create_download(uri, flags, commands);
-    return;
+    result.push_back(try_create_download(uri, flags, commands));
+    return rawResult;
   }
 
   std::vector<std::string> paths;
@@ -525,14 +540,15 @@ Manager::try_create_download_expand(const std::string& uri,
 
   path_expand(&paths, uri);
 
-  if (!paths.empty())
-    for (std::vector<std::string>::iterator itr = paths.begin();
-         itr != paths.end();
-         ++itr)
-      try_create_download(*itr, flags, commands);
+  if (!paths.empty()) {
+    for (const auto& path : paths) {
+      result.push_back(try_create_download(path, flags, commands));
+    }
+  } else {
+    result.push_back(try_create_download(uri, flags, commands));
+  }
 
-  else
-    try_create_download(uri, flags, commands);
+  return rawResult;
 }
 
 // DownloadList's hashing related functions don't actually start the

include/core/download_factory.h
@@ -39,6 +39,9 @@ public:
   command_list_type& commands() {
     return m_commands;
   }
+  torrent::Object::string_type& result() {
+    return m_result;
+  }
   torrent::Object::map_type& variables() {
     return m_variables;
   }
@@ -94,6 +97,7 @@ private:
   bool m_loaded{ false };
 
   std::string m_uri;
+  std::string m_result;
   bool        m_session{ false };
   bool        m_start{ false };
   bool        m_printLog{ true };

include/core/manager.h
@@ -125,10 +125,10 @@ public:
   using command_list_type = std::vector<std::string>;
 
   // Temporary, find a better place for this.
-  void try_create_download(const std::string&       uri,
-                           int                      flags,
-                           const command_list_type& commands);
-  void try_create_download_expand(
+  torrent::Object try_create_download(const std::string&       uri,
+                                      int                      flags,
+                                      const command_list_type& commands);
+  torrent::Object try_create_download_expand(
     const std::string& uri,
     int                flags,
     command_list_type  commands = command_list_type());

src/command_events.cc
@@ -212,9 +212,7 @@ apply_load(const torrent::Object::list_type& args, int flags) {
   while (++argsItr != args.end())
     commands.push_back(argsItr->as_string());
 
-  control->core()->try_create_download_expand(filename, flags, commands);
-
-  return torrent::Object();
+  return control->core()->try_create_download_expand(filename, flags, commands);
 }
 
 void

src/core/download_factory.cc
@@ -15,6 +15,7 @@
 #include <torrent/utils/log.h>
 #include <torrent/utils/path.h>
 #include <torrent/utils/resume.h>
+#include <torrent/utils/string_manip.h>
 
 #include "rpc/parse_commands.h"
 
@@ -380,6 +381,11 @@ DownloadFactory::receive_success() {
       download->set_message(msg);
       //     m_manager->download_list()->erase(m_manager->download_list()->find(infohash.data()));
     }
+  }
+
+  if (m_immediate) {
+    m_result = std::move(
+      torrent::utils::transform_hex_str<torrent::HashString>(infohash));
   }
 
   m_slot_finished();

commit 45d6e3c763499ff77c54ac43cb864de47d8efad5

apply_load: return "0" instead of the array for asynchronous request
Fixes: 2f286f939
Bug: #16

src/core/manager.cc
@@ -532,20 +532,23 @@ Manager::try_create_download_expand(const std::string& uri,
 
   if (flags & create_raw_data) {
     result.push_back(try_create_download(uri, flags, commands));
-    return rawResult;
+  } else {
+    std::vector<std::string> paths;
+
+    path_expand(&paths, uri);
+
+    if (!paths.empty()) {
+      for (const auto& path : paths) {
+        result.push_back(try_create_download(path, flags, commands));
+      }
+    } else {
+      result.push_back(try_create_download(uri, flags, commands));
+    }
   }
 
-  std::vector<std::string> paths;
-  paths.reserve(256);
-
-  path_expand(&paths, uri);
-
-  if (!paths.empty()) {
-    for (const auto& path : paths) {
-      result.push_back(try_create_download(path, flags, commands));
-    }
-  } else {
-    result.push_back(try_create_download(uri, flags, commands));
+  if (result.size() == 0 || result[0].type() != torrent::Object::TYPE_STRING) {
+    // return "0" instead of the array if the request is asynchronous
+    return torrent::Object();
   }
 
   return rawResult;