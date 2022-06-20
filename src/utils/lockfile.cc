8c426e55e2adb3a4a81239188e8a260e8e163106
Aleksa Sarai <cyphar@cyphar.com>
2022-06-20 19:09:57 +1000

    utils: lockfile: avoid stack overflow for lockfile buffer
    There appears to have been some change on openSUSE (likely some new
    hardening flags for builds, or some glibc hardening) such that incorrect
    buffer handling results in a segfault even if the buffer is never
    overflowed.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- src/utils/lockfile.cc
+++ src/utils/lockfile.cc
@@ -75,8 +75,9 @@ Lockfile::try_lock() {
   int  pos = ::gethostname(buf, 255);
 
   if (pos == 0) {
-    ::snprintf(buf + std::strlen(buf), 255, ":+%i\n", ::getpid());
-    ssize_t __attribute__((unused)) result = ::write(fd, buf, std::strlen(buf));
+    ssize_t len = std::strlen(buf);
+    ::snprintf(buf + len, 255 - len, ":+%i\n", ::getpid());
+    int __attribute__((unused)) result = ::write(fd, buf, std::strlen(buf));
   }
 
   ::close(fd);
